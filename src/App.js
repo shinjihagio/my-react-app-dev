f3cf031b54a2c644a03fbf6c07ea6797c022c7c8
shinji hagio <shinjihagio@gmail.com>
2020-09-09 15:38:04 +0000

    update src/App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello git</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'shinji', age: 30 },
+      { name: 'eriko', age: 29 },
+      { name: 'chinatsu', age: 2 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: 'hagio', age: 30 },
+        { name: 'eriko', age: 29 },
+        { name: 'chinatsu', age: 5 }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello git</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Watching TV</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
-export default App;
+  export default App;
